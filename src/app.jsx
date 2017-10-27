e56e25674779aaf817cf67ccef61b9d43e4d85d8
George W Langham <georgelangham1990@gmail.com>
2017-10-27 15:14:46 +0100

    start working on main app

--- src/app.jsx
+++ src/app.jsx
@@ -9,4 +9,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var classNames = require('classnames');
+// classnames lib makes it easier to build classes which use dynamic data
 var Api = require('./utils/api');
+/* API is custom class that just does one thing: takes the data from api,
+  transforms it into a usable format.
+  By keeping it in a seperate class file though means can change the API used without breaking the main app. */
+  /*
+  ██    ██  █████  ██████  ███████
+  ██    ██ ██   ██ ██   ██ ██
+  ██    ██ ███████ ██████  ███████
+   ██  ██  ██   ██ ██   ██      ██
+    ████   ██   ██ ██   ██ ███████
+  */
+var query = ''; //used to process query at a later point
+var cityWeather = []; //Cache from API
+var currentCity = '';
