5dcda4fd59f8d3a276d9db70d87e88556cce6a81
George W Langham <georgelangham1990@gmail.com>
2017-10-27 16:06:03 +0100

    api class made. base sass, added render functions

--- src/app.jsx
+++ src/app.jsx
@@ -14,7 +14,7 @@ var Api = require('./utils/api');
 /* API is custom class that just does one thing: takes the data from api,
   transforms it into a usable format.
   By keeping it in a seperate class file though means can change the API used without breaking the main app. */
-  /*
+/*
   ██    ██  █████  ██████  ███████
   ██    ██ ██   ██ ██   ██ ██
   ██    ██ ███████ ██████  ███████
@@ -24,3 +24,56 @@ var Api = require('./utils/api');
 var query = ''; //used to process query at a later point
 var cityWeather = []; //Cache from API
 var currentCity = '';
+//
+//
+// REACT Components
+//
+//
+var Weather = React.createClass({
+  render: function() {
+    // Build class names with dynamic data
+    var weatherClass = classNames('wi wi-owm-' + this.state.weather);
+    var bgColorClass = 'weather-widget '; // very-warm, warm, normal, cold, very-cold
+
+    // Set the background colour based on the temperature
+    if (this.state.temp >= 30) {
+      bgColorClass += 'very-warm';
+    } else if (this.state.temp > 20 && this.state.temp < 30) {
+      bgColorClass += 'warm';
+    } else if (this.state.temp > 10 && this.state.temp < 20) {
+      bgColorClass += 'normal';
+    } else if (this.state.temp > 0 && this.state.temp < 10) {
+      bgColorClass += 'cold';
+    } else if (this.state.temp <= 0) {
+      bgColorClass += 'very-cold';
+    }
+
+    // Render the DOM elements
+    return <div className={bgColorClass}>
+      <h1 className="city">{cities[currentCity]}</h1>
+      <div className="weather">
+        <i className={weatherClass}></i>
+      </div>
+      <section className="weather-details">
+        <div className="temp">
+          <span className="temp-number">{this.state.temp}</span>
+          <span className="wi wi-degrees"></span>
+        </div>
+        <div className="humidity">
+          <i className="wi wi-raindrop"></i>{this.state.humidity}
+          %</div>
+        <div className="wind">
+          <i className="wi wi-small-craft-advisory">{this.state.wind}
+            <span className="vel">Km/h</span>
+          </div>
+        </section>
+      </div>
+
+      } }); // assign component to DOM var element = React.createElement(Weather, {});
+    ReactDOM.render(element, document.querySelector('.container'))
+    //
+    // React Props
+    //
+    getInitialState : function() {
+      return {weather: '', temp: 0, humidity: 0, wind: 0}
+}
